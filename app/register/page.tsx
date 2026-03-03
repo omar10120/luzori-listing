15da5d5861cae6d0756252ad797a01532f055010
tamer tamer <amr.dawoodi@hotmail.com>
2026-03-03 16:09:02 +0300

    update register button

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -200,7 +200,7 @@ const RegisterPage = () => {
                                 <span className="text-sm font-medium text-gray-300">Logo Center</span>
                                 <label className="group relative flex flex-col items-center justify-center w-full h-32 bg-white/5 border-2 border-dashed border-white/20 rounded-2xl cursor-pointer hover:bg-white/10 hover:border-[#d4af37]/50 transition-all overflow-hidden">
                                     {logo ? (
-                                        <Image src={URL.createObjectURL(logo)} className="absolute inset-0 w-full h-full object-contain" alt="Logo preview" width={200} height={200} />
+                                        <Image src={URL.createObjectURL(logo)} className="absolute inset-0 w-full h-full object-fit" alt="Logo preview" width={200} height={200} />
                                     ) : (
                                         <div className="flex flex-col items-center">
                                             <Camera className="text-gray-400 group-hover:text-[#d4af37] mb-2 transition-colors " />
@@ -216,7 +216,7 @@ const RegisterPage = () => {
                                 <span className="text-sm font-medium text-gray-300">Primary Image</span>
                                 <label className="group relative flex flex-col items-center justify-center w-full h-32 bg-white/5 border-2 border-dashed border-white/20 rounded-2xl cursor-pointer hover:bg-white/10 hover:border-[#d4af37]/50 transition-all overflow-hidden  ">
                                     {primaryImage ? (
-                                        <Image src={URL.createObjectURL(primaryImage)} className="absolute inset-0 w-full h-full object-contain" width={200} height={200} alt="Primary preview" />
+                                        <Image src={URL.createObjectURL(primaryImage)} className="absolute inset-0 w-full h-full object-fit" width={200} height={200} alt="Primary preview" />
                                     ) : (
                                         <div className="flex flex-col items-center">
                                             <Upload className="text-gray-400 group-hover:text-[#d4af37] mb-2 transition-colors" />

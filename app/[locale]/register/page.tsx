b10df4ca6dc69b46013a95d30697fd3550e2eaf5
tamer tamer <amr.dawoodi@hotmail.com>
2026-04-28 13:50:32 +0300

    update text size

--- app/[locale]/register/page.tsx
+++ app/[locale]/register/page.tsx
@@ -325,6 +325,8 @@ const RegisterPage = () => {
     <input
       type={showPassword ? "text" : "password"}
       placeholder={t("password")}
+      minLength={9}
+      maxLength={10}
       className={cn(
         "h-9 w-full rounded-xl border border-[#E9E2D8] bg-[#F2E8DC] text-[16px] text-[#22403F] placeholder:text-[#8B9A9A] focus:outline-none focus:ring-2 focus:ring-[#225D5C]/30",
         isArabic
